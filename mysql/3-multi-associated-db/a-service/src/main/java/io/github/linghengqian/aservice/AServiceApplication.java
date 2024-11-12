270f0f768f25baded8aab332699ea720cc81f93b
linghengqian <linghengqian@outlook.com>
2024-11-12 18:42:28 +0800

    Simplify the business logic of `3-multi-associated-db`

--- mysql/3-multi-associated-db/a-service/src/main/java/io/github/linghengqian/aservice/AServiceApplication.java
+++ mysql/3-multi-associated-db/a-service/src/main/java/io/github/linghengqian/aservice/AServiceApplication.java
@@ -17,6 +17,7 @@ import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,23 +47,7 @@ public class AServiceApplication {
                         status VARCHAR(50),
                         PRIMARY KEY (order_id)
                     )""").update();
-            jdbcClient.sql("""
-                    CREATE TABLE IF NOT EXISTS t_order_item (
-                        order_item_id BIGINT NOT NULL AUTO_INCREMENT,
-                        order_id BIGINT NOT NULL,
-                        user_id INT NOT NULL,
-                        phone VARCHAR(50),
-                        status VARCHAR(50),
-                        PRIMARY KEY (order_item_id)
-                    )""").update();
-            jdbcClient.sql("""
-                    CREATE TABLE IF NOT EXISTS t_address (
-                    address_id BIGINT NOT NULL,
-                    address_name VARCHAR(100) NOT NULL,
-                    PRIMARY KEY (address_id))""").update();
             jdbcClient.sql("TRUNCATE TABLE t_order").update();
-            jdbcClient.sql("TRUNCATE TABLE t_order_item").update();
-            jdbcClient.sql("TRUNCATE TABLE t_address").update();
             IntStream.rangeClosed(1, 10).forEachOrdered(i -> {
                 KeyHolder keyHolder = new GeneratedKeyHolder();
                 jdbcClient.sql("INSERT INTO t_order (user_id, order_type, address_id, status) VALUES (?, ?, ?, ?)")
@@ -74,29 +59,20 @@ public class AServiceApplication {
                 Number orderIdKey = keyHolder.getKey();
                 Objects.requireNonNull(orderIdKey);
                 long orderId = orderIdKey.longValue();
-                jdbcClient.sql("INSERT INTO t_order_item (order_id, user_id, phone, status) VALUES (?, ?, ?, ?)")
-                        .param(1, orderId)
-                        .param(2, i)
-                        .param(3, "13800000001")
-                        .param(4, "INSERT_TEST")
-                        .update();
-                jdbcClient.sql("INSERT INTO t_address (address_id, address_name) VALUES (?, ?)")
-                        .param(1, (long) i)
-                        .param(2, "address_test_" + i)
-                        .update();
+                Assert.isTrue(0 != orderId, "Generated snowflake id should never be equal to 0.");
             });
             RestClient restClient = RestClient.create();
             int port = applicationContext.getWebServer().getPort();
             restClient.get()
-                    .uri("http://127.0.0.1:" + port + "/test_all")
+                    .uri("http://127.0.0.1:" + port + "/test_select")
                     .retrieve()
                     .body(Void.class);
             restClient.post()
-                    .uri("http://127.0.0.1:" + port + "/test_transaction/spring/transaction_template")
+                    .uri("http://127.0.0.1:" + port + "/test_transaction_template")
                     .retrieve()
                     .body(Void.class);
             restClient.post()
-                    .uri("http://127.0.0.1:" + port + "/test_transaction/spring/transactional")
+                    .uri("http://127.0.0.1:" + port + "/test_transactional")
                     .retrieve()
                     .body(Void.class);
         };
@@ -104,12 +80,6 @@ public class AServiceApplication {
 }
 
 record Order(long orderId, int orderType, int userId, long addressId, String status) {
-}
-
-record OrderItem(long orderItemId, long orderId, int userId, String phone, String status) {
-}
-
-record Address(Long addressId, String addressName) {
 }
 
 @SuppressWarnings({"SqlNoDataSourceInspection", "SqlDialectInspection"})
@@ -125,57 +95,53 @@ class TestController {
         this.testService = testService;
     }
 
-    @GetMapping("/test_all")
+    @GetMapping("/test_select")
     public void testAll() {
         jdbcClient.sql("SELECT * FROM t_order").query(Order.class).list();
-        jdbcClient.sql("SELECT i.* FROM t_order o, t_order_item i WHERE o.order_id = i.order_id")
-                .query(OrderItem.class)
-                .list();
-        jdbcClient.sql("SELECT * FROM t_address").query(Address.class).list();
     }
 
-    @PostMapping("/test_transaction/spring/transaction_template")
+    @PostMapping("/test_transaction_template")
     public void testTransactionTransactionTemplate() {
         try {
             transactionTemplate.execute(new TransactionCallbackWithoutResult() {
                 @Override
                 protected void doInTransactionWithoutResult(@NonNull TransactionStatus status) {
-                    jdbcClient.sql("INSERT INTO t_order_item (order_id, user_id, phone, status) VALUES (2024, 2024, '13800000001', 'INSERT_TEST')")
+                    jdbcClient.sql("INSERT INTO t_order (user_id, order_type, address_id, status) VALUES (2024, 0, 2024, 'INSERT_TEST')")
                             .update();
-                    jdbcClient.sql("INSERT INTO t_order_item_does_not_exist (test_id_does_not_exist) VALUES (2024)")
+                    jdbcClient.sql("INSERT INTO t_order_does_not_exist (test_id_does_not_exist) VALUES (2024)")
                             .update();
                 }
             });
         } catch (Exception ignored) {
         }
-        jdbcClient.sql("SELECT * FROM t_order_item WHERE user_id = 2024")
-                .query(OrderItem.class)
+        jdbcClient.sql("SELECT * FROM t_order WHERE user_id = 2024")
+                .query(Order.class)
                 .optional()
                 .ifPresent(_ -> {
                     throw new RuntimeException("Normally, this exception should not be thrown.");
                 });
     }
 
-    @PostMapping("/test_transaction/spring/transactional")
+    @PostMapping("/test_transactional")
     public void testTransactionTransactional() {
         try {
             testService.testTransactional();
         } catch (Exception ignored) {
         }
-        jdbcClient.sql("SELECT * FROM t_order_item WHERE user_id = 2023")
-                .query(OrderItem.class)
+        jdbcClient.sql("SELECT * FROM t_order WHERE user_id = 2023")
+                .query(Order.class)
                 .optional()
                 .ifPresent(_ -> {
                     throw new RuntimeException("Normally, this exception should not be thrown.");
                 });
     }
 
-    @PostMapping("/test/provider")
+    @PostMapping("/test_provider")
     public void testProvider() {
         transactionTemplate.execute(new TransactionCallbackWithoutResult() {
             @Override
             protected void doInTransactionWithoutResult(@NonNull TransactionStatus status) {
-                jdbcClient.sql("INSERT INTO t_order_item (order_id, user_id, phone, status) VALUES (5201314, 5201314, '13800000001', 'INSERT_TEST')")
+                jdbcClient.sql("INSERT INTO t_order (user_id, order_type, address_id, status) VALUES (5201314, 0, 5201314, 'INSERT_TEST')")
                         .update();
             }
         });
@@ -193,9 +159,9 @@ class TestService {
 
     @Transactional
     public void testTransactional() {
-        jdbcClient.sql("INSERT INTO t_order_item (order_id, user_id, phone, status) VALUES (2023, 2023, '13800000001', 'INSERT_TEST')")
+        jdbcClient.sql("INSERT INTO t_order (user_id, order_type, address_id, status) VALUES (2023, 2, 2023, 'INSERT_TEST')")
                 .update();
-        jdbcClient.sql("INSERT INTO t_order_item_does_not_exist (test_id_does_not_exist) VALUES (2023)")
+        jdbcClient.sql("INSERT INTO t_order_does_not_exist (test_id_does_not_exist) VALUES (2023)")
                 .update();
     }
 }
